d5aea7dbc0cb3667f800712d187c858c24cad536
James Whitney <jwhitn01@calpoly.edu>
2018-09-09 22:27:00 -0700

    better test

--- glusterTesting/wordCountGluster/testScript.sh
+++ glusterTesting/wordCountGluster/testScript.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo TESTING SIZE 1;
 for i in `seq 1 24`;
 do
 ./wordCountGluster 1 $i 14 0
+done
+echo TESTING SIZE 100;
+for i in `seq 1 24`;
+do
+./wordCountGluster 100 $i 14 0
 done
