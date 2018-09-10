a3ef0fa4629a1d2a514574e8f3d57dc744335bd3
James Whitney <jwhitn01@calpoly.edu>
2018-09-09 22:26:08 -0700

    more machines

--- glusterTesting/wordCountGluster/testScript.sh
+++ glusterTesting/wordCountGluster/testScript.sh
@@ -1,23 +1,5 @@
-./wordCountGluster 1 1 14 0;
-./wordCountGluster 1 2 14 0;
-./wordCountGluster 1 4 14 0;
-./wordCountGluster 1 8 14 0;
-./wordCountGluster 1 16 14 0;
-
-./wordCountGluster 100 1 14 0;
-./wordCountGluster 100 2 14 0;
-./wordCountGluster 100 4 14 0;
-./wordCountGluster 100 8 14 0;
-./wordCountGluster 100 16 14 0;
-
-./wordCountGluster 10000 1 14 0;
-./wordCountGluster 10000 2 14 0;
-./wordCountGluster 10000 4 14 0;
-./wordCountGluster 10000 8 14 0;
-./wordCountGluster 10000 16 14 0;
-
-./wordCountGluster 10000 1 14 0;
-./wordCountGluster 10000 2 14 0;
-./wordCountGluster 10000 4 14 0;
-./wordCountGluster 10000 8 14 0;
-./wordCountGluster 10000 16 14 0;
+#!/bin/bash
+for i in `seq 1 24`;
+do
+./wordCountGluster 1 $i 14 0
+done
