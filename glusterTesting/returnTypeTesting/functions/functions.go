bcc12699b130872a30d176c02ea226b37492e99a
James Whitney <jwhitn01@calpoly.edu>
2018-09-09 21:38:09 -0700

    fixesss

--- glusterTesting/returnTypeTesting/functions/functions.go
+++ glusterTesting/returnTypeTesting/functions/functions.go
@@ -2,7 +2,7 @@ package main
 
 func ReturnMap(inputMap map[string]int) map[string]int {
 
-	var output map[string]int
+	output := make(map[string]int)
 	output["test"] = inputMap["test"]
 	return output
 }
