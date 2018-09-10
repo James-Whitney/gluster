5398e62bbc257faad3c8ec816070fb264287e325
James Whitney <jwhitn01@calpoly.edu>
2018-09-09 22:09:45 -0700

    test script added

---
+++ glusterTesting/wordCountGluster/testScript.sh
@@ -0,0 +1,23 @@
+./wordCountGluster 1 1 14 0;
+./wordCountGluster 1 2 14 0;
+./wordCountGluster 1 4 14 0;
+./wordCountGluster 1 8 14 0;
+./wordCountGluster 1 16 14 0;
+
+./wordCountGluster 100 1 14 0;
+./wordCountGluster 100 2 14 0;
+./wordCountGluster 100 4 14 0;
+./wordCountGluster 100 8 14 0;
+./wordCountGluster 100 16 14 0;
+
+./wordCountGluster 10000 1 14 0;
+./wordCountGluster 10000 2 14 0;
+./wordCountGluster 10000 4 14 0;
+./wordCountGluster 10000 8 14 0;
+./wordCountGluster 10000 16 14 0;
+
+./wordCountGluster 10000 1 14 0;
+./wordCountGluster 10000 2 14 0;
+./wordCountGluster 10000 4 14 0;
+./wordCountGluster 10000 8 14 0;
+./wordCountGluster 10000 16 14 0;
